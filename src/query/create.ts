527ce0fcf4c13910a49e74c45a0f82acfa0bf61f
TheBoringDude <iamcoderx@gmail.com>
2021-05-09 21:03:41 +0800

    push

--- src/query/create.ts
+++ src/query/create.ts
@@ -1,5 +1,6 @@
 import { Create, Collection, If, Exists, Expr } from "faunadb";
 import { DataProps } from "../utils";
+import { ExecuteIfNotExists } from "./execute";
 
 /**
  * Create a new data.
@@ -15,13 +16,14 @@ function CreateData(collection: string, data: DataProps) {
 
 /**
  * Create Data if the reference doesn't exist.
+ * (currently having webpack error)
  *
  * @param  {Expr} ref
  * @param  {string} collection
  * @param  {DataProps} data
  */
 function CreateIfNotExists(ref: Expr, collection: string, data: DataProps) {
-	return If(Exists(ref), false, Create(Collection(collection), { data }));
+	return ExecuteIfNotExists(ref, Create(Collection(collection), { data }));
 }
 
 // export functions
